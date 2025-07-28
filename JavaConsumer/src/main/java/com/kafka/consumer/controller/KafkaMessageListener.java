9a8a52c278f9c60a5669b0bd435457853b8ffa6b
gaurav700 <jangidg0786@gmail.com>
2025-07-28 18:20:32 -0400

    Making 3-4 consumers that consume data from the same topic

--- JavaConsumer/src/main/java/com/kafka/consumer/controller/KafkaMessageListener.java
+++ JavaConsumer/src/main/java/com/kafka/consumer/controller/KafkaMessageListener.java
@@ -3,12 +3,27 @@ package com.kafka.consumer.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
 
+@Service
 public class KafkaMessageListener {
     Logger log = LoggerFactory.getLogger(KafkaMessageListener.class);
     
-    @KafkaListener(topics = "test-topic", groupId = "tt-group-1")
-    public void consume(String message){
-        log.info("Consumer consume the message {}"+ message);
+    @KafkaListener(topics = "test-topic", groupId = "test-topic-1")
+    public void consume1(String message){
+        log.info("Consumer1 consume the message { " + message + " }" );
     }
+
+    @KafkaListener(topics = "test-topic", groupId = "test-topic-11")
+    public void consume2(String message){
+        log.info("Consumer2 consume the message { " + message + " }" );
+    }
+
+
+    @KafkaListener(topics = "test-topic", groupId = "test-topic-1")
+    public void consume3(String message){
+        log.info("Consumer3 consume the message { " + message + " }" );
+    }
+
+
 }
