ce067223a09c9e31024f7f3ed3809bfa5fee02a4
gaurav700 <jangidg0786@gmail.com>
2025-07-29 18:33:33 -0400

    consumer is also consuiming for a particular partion

--- JavaConsumer/src/main/java/com/kafka/consumer/controller/KafkaMessageListener.java
+++ JavaConsumer/src/main/java/com/kafka/consumer/controller/KafkaMessageListener.java
@@ -3,6 +3,7 @@ package com.kafka.consumer.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.annotation.TopicPartition;
 import org.springframework.stereotype.Service;
 
 import com.kafka.consumer.dto.Customer;
@@ -15,7 +16,8 @@ public class KafkaMessageListener {
     @KafkaListener(
       topics = "default-topic",
       groupId = "tt-1",
-      containerFactory = "stringKafkaListenerContainerFactory"
+      containerFactory = "stringKafkaListenerContainerFactory",
+      topicPartitions = {@TopicPartition(topic="default-topic", partitions = {"2"})}
     )
     public void consumeText(String message) {
         log.info("Text consumer got → {}", message);
